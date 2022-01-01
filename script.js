39288f35ee135ded8c3e6a4253bc7ca8f0bf1a2d
Tony Linz <tonyllinz@gmail.com>
2022-01-01 15:00:37 -0600

    add 9 html task sections and js pseudo code

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //use moment.js to display day in header p tag with class lead, id currentDay
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-//create 9 time block divs using bootstrap to give three columns. one for time of day,
-//which will always be the same, one for the form and one for the save buttons. save
-//div contents to local storage.
+//div contents to local storage. empty local storage each day
 
 //at the beginning of every day (midnight), change class to future color (green) 
 //background and listen for the hour to pass before giving past present or future colors
-
+var changeDay = function(event) {
+    //empty .task divs
+    //save empty .task divs to local storage
+}
 //as each hour passes, update the color of the time blocks
 
+//create event listener hopefully with moment, so when the day changes, changeDay executes
