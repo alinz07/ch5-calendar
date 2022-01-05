20c8bb9879322ac86f2a8341cd050c25235b7d4f
Tony Linz <tonyllinz@gmail.com>
2022-01-04 22:48:54 -0600

    save text of tasks to an array in local storage

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+//create task array to save tasks to local storage
+var taskArray = [];
+
 //use moment.js to display day in header p tag with class lead, id currentDay
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
+//create function to load tasks saved in local storage. Off the top of me head I think
+//it will involved iterating and parsing through an array, then appending to the 
+//element with the i id.
+var loadTasks
+
 //div contents to local storage. empty local storage each day
+//save contents to array object
+var saveTasks = function() {
+
+    for (var i = 0; i<9; i++) {
+        className = ".hour-" + (9+i);
+        taskSection = "section" + className;
+        taskDiv = $(taskSection).find(".task");
+        taskDiv.attr("taskId", i);
+        console.log(taskDiv.attr("taskId"));
+        taskText = $(taskDiv).text();
+        taskArray.push(taskText);
+    }
+
+    localStorage.setItem("tasks", taskArray);
+}
 
 //at the beginning of every day (midnight), change class to future color (green) 
 //background and listen for the hour to pass before giving past present or future colors
@@ -12,3 +35,5 @@ var changeDay = function(event) {
 //as each hour passes, update the color of the time blocks
 
 //create event listener hopefully with moment, so when the day changes, changeDay executes
+
+saveTasks();
