15b859d6c37cfda1a5c669a7b162a6cfa2ec8083
Tony Linz <tonyllinz@gmail.com>
2022-01-05 23:01:40 -0600

    changeDay will empty the inputs and change the color to future bg-color. add many pseudo functions

--- script.js
+++ script.js
@@ -25,10 +25,8 @@ var loadTasks = function() {
             inputEl.val(inputText);
         }
     }
-
 }
 
-//div contents to local storage. empty local storage each day
 //save contents to array object
 var saveTasks = function(event) {
 
@@ -48,17 +46,82 @@ var saveTasks = function(event) {
     localStorage.setItem("tasks", JSON.stringify(taskArray));
 }
 
-//at the beginning of every day (midnight), change class to future color (green) 
+//empty local storage each day
+//at the beginning of every day (midnight), empty local storage, change class to future color (green) 
 //background and listen for the hour to pass before giving past present or future colors
 var changeDay = function(event) {
-    //empty .task divs
-    //save empty .task divs to local storage
-}
-//as each hour passes, update the color of the time blocks
+    
+    for (var i = 0; i<9; i++) {
+        var className = ".hour-" + (9+i);
+        var taskSection = "section" + className;
+        var inputEl = $(taskSection).find(".task-input");
+        inputEl.val("");
+    }
 
-//create event listener hopefully with moment, so when the day changes, changeDay executes
+    //save empty .task divs to local storage and load empty inputs
+    saveTasks();
+    loadTasks();
+
+    var taskInputEl=$(".task-input");
+    taskInputEl.addClass("future");
+}
+
+
+
+//as each hour passes, update the color of the time blocks
+var setTimeCheck = function() {
+
+    var zeroMinute = 0;
+
+    //get minute of now
+    var now = moment().minute();
+
+    //get difference in minutes of now vs 00 minutes
+    var timeDiff = now - zeroMinute;
+
+    //set a timer to run the hourCheck function in the difference of minutes
+
+}
+
+var hourCheck = function(firstRun) {
+
+    //check if the hour is midnight
+
+    //if it is, run changeDay function
+
+    // else {
+    //     assignTimeColors();
+    // }
+
+    //run again every 60 minutes
+}
+
+var assignTimeColors = function () {
+
+    for (var i = 0; i<9; i++) {
+        var className = ".hour-" + (9+i);
+        var taskSection = "section" + className;
+        var inputEl = $(taskSection).find(".task-input");
+
+        //if (moment().hr == className -.hour) {
+            //then assign present class
+        //}
+        // else if () {
+
+        // }
+        // else () {
+
+        // }
+        
+        inputEl.addClass("future");
+    }
+}
+
+
 
 //create event listener to edit .task div when you click on the .task-form form
 $(".saveBtn").on("click", saveTasks);
 
 loadTasks();
+
+//assignTimeColors();
