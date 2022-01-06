22fdd527d6ea67e43b1fa568ba97e6ef7d69dc8f
Tony Linz <tonyllinz@gmail.com>
2022-01-05 18:51:47 -0600

    savetasks and loadtasks work again without the div and i can get the input value

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ var taskArray = [];
 //use moment.js to display day in header p tag with class lead, id currentDay
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
+//function to dynamically edit .task divs anytime you click on the form
+var editTask = function(event) {
+        
+    //target the div
+    //var thisInput=$(event.target);
+ 
+    
+}
+
 //create function to load tasks saved in local storage. Off the top of me head I think
 //it will involve iterating and parsing through an array, then appending to the 
 //element with the i id.
 var loadTasks = function() {
     taskArray = JSON.parse(localStorage.getItem("tasks"));
+    console.log(taskArray);
 }
 
 //div contents to local storage. empty local storage each day
@@ -16,16 +26,14 @@ var loadTasks = function() {
 var saveTasks = function() {
 
     for (var i = 0; i<9; i++) {
-        className = ".hour-" + (9+i);
-        taskSection = "section" + className;
-        taskDiv = $(taskSection).find(".task");
-        taskDiv.text("hello");
-        taskText = $(taskDiv).text();
-        const hourObj = {
+        var className = ".hour-" + (9+i);
+        var taskSection = "section" + className;
+        var taskInput = $(taskSection).find(".task-input");
+        const taskObj = {
             taskId: i+9,
-            text: taskText,
+            text: taskInput.val(),
         }
-        taskArray.push(hourObj);
+        taskArray.push(taskObj);
     }
 
     localStorage.setItem("tasks", JSON.stringify(taskArray));
@@ -40,3 +48,8 @@ var changeDay = function(event) {
 //as each hour passes, update the color of the time blocks
 
 //create event listener hopefully with moment, so when the day changes, changeDay executes
+
+//create event listener to edit .task div when you click on the .task-form form
+$(".task-input").on("click", editTask);
+
+saveTasks();
